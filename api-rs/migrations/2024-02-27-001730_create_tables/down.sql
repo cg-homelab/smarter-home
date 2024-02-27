a4ccbf62d86a295ee08146691bd876ad4a932623
Martin Ellegård <martin.ellegard@gmail.com>
2024-02-27 01:21:39 +0100

    Added migration

---
+++ api-rs/migrations/2024-02-27-001730_create_tables/down.sql
@@ -0,0 +1,6 @@
+-- This file should undo anything in `up.sql`
+DROP TABLE IF EXISTS "electricity_deals";
+DROP TABLE IF EXISTS "users";
+DROP TABLE IF EXISTS "homes";
+DROP TABLE IF EXISTS "consumption_metrics";
+DROP TABLE IF EXISTS "electricity_price";
