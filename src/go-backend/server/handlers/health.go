55798bc454db26130ed962669876467ab2bb8f7f
Martin Ellegård <martin.ellegard@gmail.com>
2025-09-03 08:01:55 +1000

    Removed old depricated code

--- src/go-backend/server/handlers/health.go
+++
@@ -1,7 +0,0 @@
-package handlers
-
-import "github.com/gofiber/fiber/v2"
-
-func (h *Handlers) HealthHandler(c *fiber.Ctx) error {
-	return c.JSON(h.db.Health())
-}
--- src/go-backend/server/handlers/homes.go
+++
@@ -1,9 +0,0 @@
-package handlers
-
-// func (s *FiberServer) GetHomes(c *fiber.Ctx) error {
-// 	resp := fiber.Map{
-// 		"message": "Hello World",
-// 	}
-//
-// 	return c.JSON(resp)
-// }
